730fcf50c61659b358342bd8e1e0b3bcf40d7a2d
Casey LaMothe <lamothe.casey@gmail.com>
2018-09-17 16:48:48 -0600

    php test

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<html>
+    <head>
+        <title>Hello World</title>
+        <link href="style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="hello">Hello World!</div>
+        <?php 
+            print("hello php world!")
+        ?>
+    </body>
+</html>
