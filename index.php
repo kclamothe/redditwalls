d9a5bfd44a4fb6a538d6c2a829a1376911d781c0
Casey LaMothe <lamothe.casey@gmail.com>
2018-09-21 09:33:52 -0600

    title change, github test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Hello World</title>
+        <title>reddit walls</title>
         <link href="style.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
