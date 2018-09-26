997ee4a3dbf9af69c2ca9fe874128f6a510d2eba
Casey LaMothe <lamothe.casey@gmail.com>
2018-09-26 00:48:20 -0600

    first implementation of website

---
+++ header.php
@@ -0,0 +1,9 @@
+<header id="navbar">
+    <div class="logo"><a href="index.php"><img src="images/logo.png"></a></div>
+    <nav class="menu">
+        <ul>
+            <li><a href="favorites.php">Favorites</a></li>
+            <li><a href="login.php">Login/Register</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ testimages.php
@@ -0,0 +1,159 @@
+<!-- column 1 -->
+<div class="image_column">
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillu</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+</div>
+
+<!-- column 2 -->
+<div class="image_column">
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+</div>
+
+<!-- column 3 -->
+<div class="image_column">
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+</div>
+
+<!-- column 4 -->
+<div class="image_column">
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/te09stgz9sl11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.imgur.com/Iuad5ib.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="https://i.redd.it/w0z5aufrs6o11.jpg">
+        <div class="info">
+            <div class="title">This is the title of the image</div>
+        </div>
+    </div>
+</div>
