96359aa38d7cf7b1882b519ea7bc0c6d86758650
Casey LaMothe <lamothe.casey@gmail.com>
2018-09-26 00:48:03 -0600

    first implementation of website

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
     <head>
         <title>reddit walls</title>
         <link href="style.css" type="text/css" rel="stylesheet" />
+        <link href="images/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
     </head>
     <body>
-        <div id="hello">Hello World!</div>
-        <?php 
-            print("hello php world!")
-        ?>
+        <?php require_once "header.php"; ?>
+        <div id="spacer"></div>
+        <div id="content">
+            <?php require_once "testimages.php"; ?>
+        </div>
+        <div id="navigation">
+            <a href="#" class="navbutton">< Previous</a>
+            <a href="#" class="navbutton">Next ></a>  
+        </div>
+        <div id="footer">©2018 K. Casey LaMothe</div>
     </body>
 </html>
