18a48b57b94aa8e8a9a4bae01cbbaab933c67e20
Casey LaMothe <lamothe.casey@gmail.com>
2018-11-05 16:41:21 -0700

    favoritewallpaper.php added

---
+++ favoritewallpapers.php
@@ -0,0 +1,57 @@
+<?php
+    session_start();
+    require_once 'DAO.php';
+    $dao = new DAO();
+?>
+
+<div class="image_column">
+
+    <?php
+    $wallpapers = $dao->getFavorites($page, $user);
+    $rowCount = 0;
+    foreach ($wallpapers as $wallpaper) {
+        if ($rowCount%2==0){
+            echo "
+                <div class=\"container\">
+                    <a href=\"" . $wallpaper['image_link'] . "\" target=\"_blank\"><img src=\"" . $wallpaper['image_link'] . "\"></a>
+                    <div class=\"info\">
+                        <div class=\"title\">
+                            <a href=\"". $wallpaper['reddit_link'] ."\" target=\"_blank\">". $wallpaper['title'] ."</a>
+                            <div class=\"fav\">
+                                Save
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+        }
+        $rowCount+=1;
+    }
+    ?>
+
+</div>
+
+<div class="image_column">
+
+    <?php
+    $wallpapers = $dao->getFavorites($page, $user);
+    $rowCount = 0;
+    foreach ($wallpapers as $wallpaper) {
+        if ($rowCount%2==1){
+            echo "
+                <div class=\"container\">
+                    <a href=\"" . $wallpaper['image_link'] . "\" target=\"_blank\"><img src=\"" . $wallpaper['image_link'] . "\"></a>
+                    <div class=\"info\">
+                        <div class=\"title\">
+                            <a href=\"". $wallpaper['reddit_link'] ."\" target=\"_blank\">". $wallpaper['title'] ."</a>
+                            <div class=\"fav\">
+                                Save
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+        }
+        $rowCount+=1;
+    }
+    ?>
+
+</div>
